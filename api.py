dc60b7b45a89ef59f2a15802244cb81a05cb3842
luizhlana <luizhlana@gmail.com>
2023-02-01 11:45:32 -0300

    Initial Commit API

---
+++ api.py
@@ -0,0 +1,18 @@
+import justpy as jp
+import definition
+
+class Api:
+    """Handles requests at /api?w=word
+    """
+    @classmethod
+    def serve(cls, req):
+        wp = jp.WebPage()
+        word = req.query_params.get('w')
+
+        defined = definition.Definition(word).get()
+
+        wp.html = defined
+        return wp
+
+jp.Route("/api", Api.serve)
+jp.justpy()
---
+++ definition.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class Definition:
+
+    def __init__(self, term):
+        self.term = term
+
+    def get(self):
+        df = pd.read_csv('data.csv')
+        return tuple(df[df['word']==self.term]['definition'])
+
+d = Definition(term='sun')
+print(d.get())
